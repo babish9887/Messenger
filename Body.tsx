d567fbe3f740c01c3fb8c16a4ebc018b48571620
babish9887 <babish9887@gmail.com>
2024-04-24 20:50:55 +0545

    removed console.logs

--- Body.tsx
+++ Body.tsx
@@ -1,72 +1,72 @@
-'use client'
-import useCoversation from '@/app/hooks/useConversation';
-import { FullMessageType } from '@/app/types'
-import React, { useEffect, useRef, useState } from 'react'
-import MessageBox from './MessageBox';
-import axios from 'axios';
-import { pusherClient } from '@/app/lib/pusher';
-import { find } from 'lodash';
+// 'use client'
+// import useCoversation from '@/app/hooks/useConversation';
+// import { FullMessageType } from '@/app/types'
+// import React, { useEffect, useRef, useState } from 'react'
+// import MessageBox from './MessageBox';
+// import axios from 'axios';
+// import { pusherClient } from '@/app/lib/pusher';
+// import { find } from 'lodash';
 
-interface BodyProps{
-      initialMessages:FullMessageType[]
-}
+// interface BodyProps{
+//       initialMessages:FullMessageType[]
+// }
 
-function Body({initialMessages}:BodyProps) {
-      const [messages, setMessages]=useState(initialMessages);
-      const bottomRef=useRef<HTMLDivElement>(null);
-      const {conversationId}=useCoversation();
+// function Body({initialMessages}:BodyProps) {
+//       const [messages, setMessages]=useState(initialMessages);
+//       const bottomRef=useRef<HTMLDivElement>(null);
+//       const {conversationId}=useCoversation();
 
-      useEffect( ()=>{
-            axios.post(`/api/conversations/${conversationId}/seen`)
-      },[conversationId]);
+//       useEffect( ()=>{
+//             axios.post(`/api/conversations/${conversationId}/seen`)
+//       },[conversationId]);
 
-      useEffect(() => {
-            pusherClient.subscribe(conversationId)
-            bottomRef?.current?.scrollIntoView();
+//       useEffect(() => {
+//             pusherClient.subscribe(conversationId)
+//             bottomRef?.current?.scrollIntoView();
         
-            const messageHandler = (message: FullMessageType) => {
-              axios.post(`/api/conversations/${conversationId}/seen`);
+//             const messageHandler = (message: FullMessageType) => {
+//               axios.post(`/api/conversations/${conversationId}/seen`);
         
-              setMessages((current) => {
-                if (find(current, { id: message.id })) {
-                  return current;
-                }
+//               setMessages((current) => {
+//                 if (find(current, { id: message.id })) {
+//                   return current;
+//                 }
         
-                return [...current, message]
-              });
+//                 return [...current, message]
+//               });
               
-              bottomRef?.current?.scrollIntoView();
-            };
+//               bottomRef?.current?.scrollIntoView();
+//             };
         
-            const updateMessageHandler = (newMessage: FullMessageType) => {
-              setMessages((current) => current.map((currentMessage) => {
-                if (currentMessage.id === newMessage.id) {
-                  return newMessage;
-                }
+//             const updateMessageHandler = (newMessage: FullMessageType) => {
+//               setMessages((current) => current.map((currentMessage) => {
+//                 if (currentMessage.id === newMessage.id) {
+//                   return newMessage;
+//                 }
           
-                return currentMessage;
-              }))
-            };
+//                 return currentMessage;
+//               }))
+//             };
           
         
-            pusherClient.bind('messages:new', messageHandler)
-            pusherClient.bind('message:update', updateMessageHandler);
+//             pusherClient.bind('messages:new', messageHandler)
+//             pusherClient.bind('message:update', updateMessageHandler);
         
-            return () => {
-              pusherClient.unsubscribe(conversationId)
-              pusherClient.unbind('messages:new', messageHandler)
-              pusherClient.unbind('message:update', updateMessageHandler)
-            }
-          }, [conversationId]);
+//             return () => {
+//               pusherClient.unsubscribe(conversationId)
+//               pusherClient.unbind('messages:new', messageHandler)
+//               pusherClient.unbind('message:update', updateMessageHandler)
+//             }
+//           }, [conversationId]);
       
-      return (
-            <div className='flex-1 overflow-y-auto'>
-                  {messages?.map((message,i)=>(
-                        <MessageBox isLast={i===messages.length-1} key={message.id} data={message} />
-                  ))}
-                  <div ref={bottomRef} className='pt-24'/>
-            </div>
-      )
-}
+//       return (
+//             <div className='flex-1 overflow-y-auto'>
+//                   {messages?.map((message,i)=>(
+//                         <MessageBox isLast={i===messages.length-1} key={message.id} data={message} />
+//                   ))}
+//                   <div ref={bottomRef} className='pt-24'/>
+//             </div>
+//       )
+// }
 
-export default Body
+// export default Body
